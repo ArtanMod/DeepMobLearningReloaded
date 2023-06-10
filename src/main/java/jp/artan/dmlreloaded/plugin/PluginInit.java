f3dae7d61f239346ec9052051a7395a47c9bce32
t.akasaki <t.akasaki.csharp@gmail.com>
2023-06-10 14:33:51 +0900

    feat: :sparkles: 外部MODの連携を機能を追加する仕組みを追加

--- src/main/java/jp/artan/dmlreloaded/plugin/PluginInit.java
+++ src/main/java/jp/artan/dmlreloaded/plugin/PluginInit.java
@@ -1,9 +1,16 @@
 package jp.artan.dmlreloaded.plugin;
 
+import jp.artan.dmlreloaded.plugin.tconstruct.TiCPlugin;
+import jp.artan.dmlreloaded.plugin.twilight.TwilightPlugin;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModList;
 
 public class PluginInit {
-    public static void init(IEventBus modEventBus){
+    public static final boolean TWILIGHT_ACTIVE = ModList.get().isLoaded(TwilightPlugin.NAME);
+    public static final boolean TIC_ACTIVE = ModList.get().isLoaded(TiCPlugin.NAME);
 
+    public static void init(IEventBus modEventBus){
+        TwilightPlugin.init(modEventBus);
+        TiCPlugin.init(modEventBus);
     }
 }
---
+++ src/main/java/jp/artan/dmlreloaded/plugin/tconstruct/TiCPlugin.java
@@ -0,0 +1,14 @@
+package jp.artan.dmlreloaded.plugin.tconstruct;
+
+import net.minecraftforge.eventbus.api.IEventBus;
+
+import static jp.artan.dmlreloaded.plugin.PluginInit.TIC_ACTIVE;
+
+public class TiCPlugin {
+    public static final String NAME = "tconstruct";
+
+    public static void init(IEventBus modEventBus) {
+        if (TIC_ACTIVE) {
+        }
+    }
+}
