7be75b87355b49f289c9b498f05f019f24a890db
t.akasaki <t.akasaki.csharp@gmail.com>
2023-06-11 02:35:45 +0900

    feat: :fire: Tinkers Constructとの連携を削除

--- src/main/java/jp/artan/dmlreloaded/plugin/PluginInit.java
+++ src/main/java/jp/artan/dmlreloaded/plugin/PluginInit.java
@@ -1,16 +1,15 @@
 package jp.artan.dmlreloaded.plugin;
 
-import jp.artan.dmlreloaded.plugin.tconstruct.TiCPlugin;
 import jp.artan.dmlreloaded.plugin.twilight.TwilightPlugin;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModList;
 
 public class PluginInit {
     public static final boolean TWILIGHT_ACTIVE = ModList.get().isLoaded(TwilightPlugin.NAME);
-    public static final boolean TIC_ACTIVE = ModList.get().isLoaded(TiCPlugin.NAME);
 
     public static void init(IEventBus modEventBus){
-        TwilightPlugin.init(modEventBus);
-        TiCPlugin.init(modEventBus);
+        if (TWILIGHT_ACTIVE) {
+            TwilightPlugin.init(modEventBus);
+        }
     }
 }
--- src/main/java/jp/artan/dmlreloaded/plugin/twilight/TwilightPlugin.java
+++ src/main/java/jp/artan/dmlreloaded/plugin/twilight/TwilightPlugin.java
@@ -2,13 +2,10 @@ package jp.artan.dmlreloaded.plugin.twilight;
 
 import net.minecraftforge.eventbus.api.IEventBus;
 
-import static jp.artan.dmlreloaded.plugin.PluginInit.TWILIGHT_ACTIVE;
-
 public class TwilightPlugin {
     public static final String NAME = "twilightforest";
 
     public static void init(IEventBus modEventBus) {
-        if (TWILIGHT_ACTIVE) {
-        }
+
     }
 }
