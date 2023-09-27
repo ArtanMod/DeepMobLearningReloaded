d21ba96d42f3a283e017a5c29a0291649d10c2e9
t.akasaki <t.akasaki.csharp@gmail.com>
2023-09-27 20:35:50 +0900

    feat: :sparkles: Reliquary Reincarnationsとの連携を追加

--- src/main/java/jp/artan/dmlreloaded/plugin/PluginInit.java
+++ src/main/java/jp/artan/dmlreloaded/plugin/PluginInit.java
@@ -1,15 +1,20 @@
 package jp.artan.dmlreloaded.plugin;
 
+import jp.artan.dmlreloaded.plugin.reliquary.ReliquaryPlugin;
 import jp.artan.dmlreloaded.plugin.twilight.TwilightPlugin;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModList;
 
 public class PluginInit {
     public static final boolean TWILIGHT_ACTIVE = ModList.get().isLoaded(TwilightPlugin.NAME);
+    public static final boolean RELIQUARY_ACTIVE = ModList.get().isLoaded(ReliquaryPlugin.NAME);
 
     public static void init(IEventBus modEventBus){
         if (TWILIGHT_ACTIVE) {
             TwilightPlugin.init(modEventBus);
+        }
+        if (RELIQUARY_ACTIVE) {
+            ReliquaryPlugin.init(modEventBus);
         }
     }
 }
