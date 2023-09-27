40e12e6377929d3fd53a6338f5eb37829f572064
t.akasaki <t.akasaki.csharp@gmail.com>
2023-09-27 22:26:29 +0900

    feat: :sparkles: DivineRPGとの連携を追加

---
+++ src/main/java/jp/artan/dmlreloaded/plugin/DIvineRPG/DivineRPGPlugin.java
@@ -0,0 +1,26 @@
+package jp.artan.dmlreloaded.plugin.DIvineRPG;
+
+import divinerpg.DivineRPG;
+import jp.artan.dmlreloaded.common.MobKey;
+import net.minecraftforge.eventbus.api.IEventBus;
+
+public class DivineRPGPlugin {
+    public static final String NAME = DivineRPG.MODID;
+
+    public static void init(IEventBus modEventBus) {
+        registerMobKey();
+    }
+
+    private static void registerMobKey() {
+        MobKey.ENDERMAN.addMob("entity.divinerpg.ender_spider");
+        MobKey.ENDERMAN.addMob("entity.divinerpg.ender_scrounge");
+        MobKey.ENDERMAN.addMob("entity.divinerpg.ender_triplets");
+        MobKey.ENDERMAN.addMob("entity.divinerpg.ender_watcher");
+
+        MobKey.SPIDER.addMob("entity.divinerpg.hell_spider");
+        MobKey.SPIDER.addMob("entity.divinerpg.jungle_spider");
+        MobKey.SPIDER.addMob("entity.divinerpg.ender_spider");
+        MobKey.SPIDER.addMob("entity.divinerpg.pumpkin_spider");
+        MobKey.SPIDER.addMob("entity.divinerpg.soul_spider");
+    }
+}
