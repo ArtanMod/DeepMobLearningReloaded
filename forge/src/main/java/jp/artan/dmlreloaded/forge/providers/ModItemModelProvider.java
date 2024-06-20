6a685b2bd0cb086e1aed856c1eee692b32c06011
t.akasaki <t.akasaki.csharp@gmail.com>
2024-06-20 20:41:41 +0900

    feat: :sparkles: アイテムモデルを追加

--- forge/src/main/java/jp/artan/dmlreloaded/forge/providers/ModItemModelProvider.java
+++ forge/src/main/java/jp/artan/dmlreloaded/forge/providers/ModItemModelProvider.java
@@ -1,6 +1,7 @@
 package jp.artan.dmlreloaded.forge.providers;
 
 import jp.artan.artansprojectcoremod.forge.providers.AbstractItemModelProvider;
+import jp.artan.dmlreloaded.init.DMLItems;
 import net.minecraft.data.DataGenerator;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
@@ -11,6 +12,51 @@ public class ModItemModelProvider extends AbstractItemModelProvider {
 
     @Override
     protected void registerModels() {
+        registerStandardItem();
+        registerDataModel();
+        registerPristineMatter();
+        registerLivingMatter();
+    }
+
+    /**
+     * 標準アイテム
+     */
+    private void registerStandardItem() {
+        this.basicItem(DMLItems.SOOT_COVERED_REDSTONE.get());
+        this.basicItem(DMLItems.SOOT_COVERED_PLATE.get());
+        this.basicItem(DMLItems.GLITCH_FRAGMENT.get());
+        this.basicItem(DMLItems.GLITCH_HEART.get());
+        this.basicItem(DMLItems.GLITCH_INGOT.get());
+        this.basicItem(DMLItems.NETHERITE_GLITCH_INGOT.get());
+        this.basicItem(DMLItems.POLYMER_CLAY.get());
+//        this.basicItem(DMLItems.GLITCH_SWORD.get());
+//        this.basicItem(DMLItems.NETHERITE_GLITCH_SWORD.get());
+    }
+
+    /**
+     * データモデル
+     */
+    private void registerDataModel() {
+        this.basicItem(DMLItems.DATA_MODEL_BLANK.get());
+        this.basicItem(DMLItems.DATA_MODEL_ZOMBIE.get());
+
+    }
+
+    /**
+     * マター
+     */
+    private void registerPristineMatter() {
+        this.basicItem(DMLItems.PRISTINE_MATTER_ZOMBIE.get());
+
+    }
+
+    /**
+     * 環境マター
+     */
+    private void registerLivingMatter() {
+        this.basicItem(DMLItems.LIVING_MATTER_EXTRATERRESTRIAL.get());
+        this.basicItem(DMLItems.LIVING_MATTER_HELLISH.get());
+        this.basicItem(DMLItems.LIVING_MATTER_OVERWORLDIAN.get());
 
     }
 }
