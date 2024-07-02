785050f843d156538f9070298e1b31749ac03e0b
t.akasaki <t.akasaki.csharp@gmail.com>
2024-07-02 22:37:29 +0900

    feat: :sparkles: クリエイティブタブのアイコンを変更

--- common/src/main/java/jp/artan/dmlreloaded/init/DMLCreativeTab.java
+++ common/src/main/java/jp/artan/dmlreloaded/init/DMLCreativeTab.java
@@ -2,12 +2,13 @@ package jp.artan.dmlreloaded.init;
 
 import dev.architectury.registry.CreativeTabRegistry;
 import jp.artan.dmlreloaded.DeepMobLearningReloadedMod;
+import net.minecraft.core.Registry;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 
 public class DMLCreativeTab {
-    public static final CreativeModeTab DEEP_MOB_LEARNING_RELOADED = CreativeTabRegistry.create(DeepMobLearningReloadedMod.getResource("deep_mob_learning_reloaded"), () -> new ItemStack(Items.APPLE));
+    public static final CreativeModeTab DEEP_MOB_LEARNING_RELOADED = CreativeTabRegistry.create(DeepMobLearningReloadedMod.getResource("deep_mob_learning_reloaded"), () -> new ItemStack(Registry.ITEM.get(DeepMobLearningReloadedMod.getResource("deep_learner"))));
     public static void register() {
     }
 }
