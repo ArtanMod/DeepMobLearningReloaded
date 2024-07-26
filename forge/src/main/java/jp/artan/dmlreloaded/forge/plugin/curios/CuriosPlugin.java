7904e7d661a5272f07c066444e85979240a14286
t.akasaki <t.akasaki.csharp@gmail.com>
2024-07-27 00:04:21 +0900

    refactor: :recycle: curiosのMOD IDを自分で定義するように変更

--- forge/src/main/java/jp/artan/dmlreloaded/forge/plugin/curios/CuriosPlugin.java
+++ forge/src/main/java/jp/artan/dmlreloaded/forge/plugin/curios/CuriosPlugin.java
@@ -1,10 +1,9 @@
 package jp.artan.dmlreloaded.forge.plugin.curios;
 
 import net.minecraftforge.eventbus.api.IEventBus;
-import top.theillusivec4.curios.api.CuriosApi;
 
 public class CuriosPlugin {
-    public static final String NAME = CuriosApi.MODID;
+    public static final String NAME = "curios";
 
     public static void init(IEventBus modEventBus) {
     }
