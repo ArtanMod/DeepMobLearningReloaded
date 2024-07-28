a733d0977506110b39ecd05f643578091a63949b
t.akasaki <t.akasaki.csharp@gmail.com>
2024-07-28 12:08:35 +0900

    feat: :sparkles: Curiosの非推奨機能対応

--- forge/src/main/java/jp/artan/dmlreloaded/forge/plugin/curios/CuriosUtil.java
+++ forge/src/main/java/jp/artan/dmlreloaded/forge/plugin/curios/CuriosUtil.java
@@ -1,39 +1,33 @@
 package jp.artan.dmlreloaded.forge.plugin.curios;
 
-import jp.artan.dmlreloaded.forge.init.DMLItemsForge;
 import jp.artan.dmlreloaded.forge.item.ItemDeepLearner;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.common.util.LazyOptional;
 import top.theillusivec4.curios.api.CuriosApi;
-import top.theillusivec4.curios.api.SlotResult;
+import top.theillusivec4.curios.api.type.capability.ICuriosItemHandler;
+import top.theillusivec4.curios.api.type.inventory.ICurioStacksHandler;
 
-import java.util.Optional;
 
 public class CuriosUtil {
+    private static final String DEEP_LEARNER_SLOT = "deep_learner";
 
     public static ItemStack getDeepLearnerCurioItemStack(Player player) {
-        ItemStack deepLearner = getDeepLearner(getDeepLearnerSlotResult(player));
-        if(deepLearner.isEmpty()) {
-            deepLearner = getDeepLearner(getNetheriteDeepLearnerSlotResult(player));
-        }
-        return deepLearner;
-    }
-
-    private static Optional<SlotResult> getDeepLearnerSlotResult(Player player) {
-        return CuriosApi.getCuriosHelper().findFirstCurio(player, DMLItemsForge.DEEP_LEARNER.get());
-    }
-
-    private static Optional<SlotResult> getNetheriteDeepLearnerSlotResult(Player player) {
-        return CuriosApi.getCuriosHelper().findFirstCurio(player, DMLItemsForge.NETHERITE_DEEP_LEARNER.get());
-    }
-
-    private static ItemStack getDeepLearner(Optional<SlotResult> slotResult) {
-        if(slotResult.isPresent()) {
-            ItemStack stack = slotResult.get().stack();
-            if(stack.getItem() instanceof ItemDeepLearner) {
-                return stack;
+        try {
+            LazyOptional<ICuriosItemHandler> curiosInventory = CuriosApi.getCuriosInventory(player);
+            if(curiosInventory.isPresent()) {
+                ICuriosItemHandler curiosItemHandler = curiosInventory.orElseThrow(IllegalStateException::new);
+                ICurioStacksHandler curioStacksHandler = curiosItemHandler.getStacksHandler(DEEP_LEARNER_SLOT).orElseThrow();
+                for(int i = 0; i < curioStacksHandler.getSlots(); i++) {
+                    ItemStack stack = curioStacksHandler.getStacks().getStackInSlot(i);
+                    if(stack.getItem() instanceof ItemDeepLearner) {
+                        return stack;
+                    }
+                }
             }
+            return ItemStack.EMPTY;
+        } catch(Exception e) {
+            return ItemStack.EMPTY;
         }
-        return ItemStack.EMPTY;
     }
 }
