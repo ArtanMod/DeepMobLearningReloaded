321bc0c497783b7829ab1cd7d0187a4cc64b45f5
a.kubo <ayuyellow1725@gmail.com>
2024-08-11 18:09:49 +0900

    fix: :bug: Deeper and Darkerマターの日本語訳を追加

--- forge/src/main/java/jp/artan/dmlreloaded/forge/providers/ModJPLanguageProvider.java
+++ forge/src/main/java/jp/artan/dmlreloaded/forge/providers/ModJPLanguageProvider.java
@@ -4,6 +4,7 @@ import jp.artan.artansprojectcoremod.forge.providers.AbstractJPLanguageProvider;
 import jp.artan.dmlreloaded.DeepMobLearningReloadedMod;
 import jp.artan.dmlreloaded.forge.init.DMLBlocksForge;
 import jp.artan.dmlreloaded.forge.init.DMLItemsForge;
+import jp.artan.dmlreloaded.forge.plugin.DeeperAndDarker.init.DeeperAndDarkerItemInit;
 import jp.artan.dmlreloaded.init.DMLCreativeTab;
 import jp.artan.dmlreloaded.init.DMLItems;
 import net.minecraft.data.DataGenerator;
@@ -320,6 +321,7 @@ public class ModJPLanguageProvider extends AbstractJPLanguageProvider {
         this.add(DMLItems.LIVING_MATTER_EXTRATERRESTRIAL.get(), "Extraterrestrialマター");
         this.add(DMLItems.LIVING_MATTER_HELLISH.get(), "Hellishマター");
         this.add(DMLItems.LIVING_MATTER_OVERWORLDIAN.get(), "Overworldianマター");
+        this.add(DeeperAndDarkerItemInit.LIVING_MATTER_DEEPER_AND_DARKER.get(), "Deeper and Darkerマター");
     }
 
 }
