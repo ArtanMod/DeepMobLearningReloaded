467ea9bc158dca468cdef0edbff948fb53d348d6
t.akasaki <t.akasaki.csharp@gmail.com>
2024-08-20 20:58:49 +0900

    feat: :sparkles: CATACLYSMを一時的に削除

--- forge/src/main/java/jp/artan/dmlreloaded/forge/plugin/PluginInit.java
+++ forge/src/main/java/jp/artan/dmlreloaded/forge/plugin/PluginInit.java
@@ -33,8 +33,8 @@ public class PluginInit {
         if (DEEP_AND_DARKER_ACTIVE) {
             DeeperAndDarkerPlugin.init();
         }
-        if (CATACLYSM_ACTIVE) {
-            CataclysmPlugin.init(modEventBus);
-        }
+//        if (CATACLYSM_ACTIVE) {
+//            CataclysmPlugin.init(modEventBus);
+//        }
     }
 }
