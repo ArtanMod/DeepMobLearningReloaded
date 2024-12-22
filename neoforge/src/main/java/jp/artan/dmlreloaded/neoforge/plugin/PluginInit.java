35f9344c05b13d51280dc46d1485dc517d3f64f3
t.akasaki <t.akasaki.csharp@gmail.com>
2024-12-22 17:29:57 +0900

    chore: :fire: 一時的にCataclysm連携を削除

--- neoforge/src/main/java/jp/artan/dmlreloaded/neoforge/plugin/PluginInit.java
+++ neoforge/src/main/java/jp/artan/dmlreloaded/neoforge/plugin/PluginInit.java
@@ -1,13 +1,12 @@
 package jp.artan.dmlreloaded.neoforge.plugin;
 
 import dev.architectury.platform.Platform;
-import jp.artan.dmlreloaded.neoforge.plugin.Cataclysm.CataclysmPlugin;
 import jp.artan.dmlreloaded.neoforge.plugin.DeeperAndDarker.DeeperAndDarkerPlugin;
 import jp.artan.dmlreloaded.neoforge.plugin.DivineRPG.DivineRPGPlugin;
 import jp.artan.dmlreloaded.neoforge.plugin.curios.CuriosPlugin;
 import jp.artan.dmlreloaded.neoforge.plugin.mekanism.MekanismPlugin;
 import jp.artan.dmlreloaded.neoforge.plugin.reliquary.ReliquaryPlugin;
-import net.minecraftforge.eventbus.api.IEventBus;
+import net.neoforged.bus.api.IEventBus;
 
 public class PluginInit {
     public static final boolean RELIQUARY_ACTIVE = Platform.isModLoaded(ReliquaryPlugin.NAME);
@@ -15,7 +14,6 @@ public class PluginInit {
     public static final boolean MEKANISM_ACTIVE = Platform.isModLoaded(MekanismPlugin.NAME);
     public static final boolean CURIOS_ACTIVE = Platform.isModLoaded(CuriosPlugin.NAME);
     public static final boolean DEEP_AND_DARKER_ACTIVE = Platform.isModLoaded(DeeperAndDarkerPlugin.NAME);
-    public static final boolean CATACLYSM_ACTIVE = Platform.isModLoaded(CataclysmPlugin.NAME);
 
     public static void init(IEventBus modEventBus) {
         if (RELIQUARY_ACTIVE) {
@@ -33,8 +31,5 @@ public class PluginInit {
         if (DEEP_AND_DARKER_ACTIVE) {
             DeeperAndDarkerPlugin.init();
         }
-//        if (CATACLYSM_ACTIVE) {
-//            CataclysmPlugin.init(modEventBus);
-//        }
     }
 }
